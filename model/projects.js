5d339274ea031e68472ba506a7b0718d1f25a49e
daniel <daniel.neveux@gmail.com>
2015-02-24 10:56:34 +0100

    step 8

--- model/projects.js
+++ model/projects.js
@@ -1,1 +1,19 @@
 Projects = new Mongo.Collection("projects");
+
+Projects.allow({
+  insert: function (userId, project) {
+    return userId && project.owner === userId;
+  },
+  update: function (userId, project, fields, modifier) {
+    if (userId !== project.owner)
+      return false;
+
+    return true;
+  },
+  remove: function (userId, project) {
+    if (userId !== project.owner)
+      return false;
+
+    return true;
+  }
+});
